19a01e07fd6312325e960d3a6aa983ebd09882b3
Marcelo de Miranda Junior <marcelomijunior@outlook.com>
2020-07-16 18:39:39 -0300

    Obtendo e salvando os dados de cadastro do usuário.

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -65,7 +65,15 @@ namespace CasaDoCodigo.Controllers
 
             var usuario = await userManager.GetUserAsync(User);
 
+            pedido.Cadastro.Nome = usuario.Nome;
             pedido.Cadastro.Email = usuario.Email;
+            pedido.Cadastro.Telefone = usuario.Telefone;
+            pedido.Cadastro.Endereco = usuario.Endereco;
+            pedido.Cadastro.Complemento = usuario.Complemento;
+            pedido.Cadastro.Bairro = usuario.Bairro;
+            pedido.Cadastro.Municipio = usuario.Municipio;
+            pedido.Cadastro.UF = usuario.UF;
+            pedido.Cadastro.CEP = usuario.CEP;
 
             return View(pedido.Cadastro);
         }
@@ -77,6 +85,20 @@ namespace CasaDoCodigo.Controllers
         {
             if (ModelState.IsValid)
             {
+                var usuario = await userManager.GetUserAsync(User);
+
+                usuario.Nome = cadastro.Nome;
+                usuario.Email = cadastro.Email;
+                usuario.Telefone = cadastro.Telefone;
+                usuario.Endereco = cadastro.Endereco;
+                usuario.Complemento = cadastro.Complemento;
+                usuario.Bairro = cadastro.Bairro;
+                usuario.Municipio = cadastro.Municipio;
+                usuario.UF = cadastro.UF;
+                usuario.CEP = cadastro.CEP;
+
+                await userManager.UpdateAsync(usuario);
+
                 return View(await pedidoRepository.UpdateCadastroAsync(cadastro));
             }
 
