cfa0361324078ce567f5caab02457e7f5a8028e6
Marcelo de Miranda Junior <marcelomijunior@outlook.com>
2020-07-16 18:21:54 -0300

    Adicionado informações de endereço, bairro, cidade, UF etc no usuário. Migração AppIdentityContext tabela AspNetUsers

--- CasaDoCodigo/Migrations/SqliteMigrations/20200716211833_UsuarioCustomizadoIdentity.Designer.cs
+++ CasaDoCodigo/Migrations/SqliteMigrations/20200716211833_UsuarioCustomizadoIdentity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CasaDoCodigo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace CasaDoCodigo.Migrations.SqliteMigrations
+namespace CasaDoCodigo.Migrations.sqliteMigrations
 {
     [DbContext(typeof(AppIdentityContext))]
-    partial class AppIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20200716211833_UsuarioCustomizadoIdentity")]
+    partial class UsuarioCustomizadoIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
