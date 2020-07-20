604af5de2d79aa6d3ea69c148c02d3b40c43dde5
Marcelo de Miranda Junior <marcelomijunior@outlook.com>
2020-07-20 14:11:26 -0300

    Configurado porta do projeto de relatório. Removido métodos getId, put e delete do controller de relatório. Adaptando os métodos post e get no controller de relatorios.

--- CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
+++ CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,31 @@ namespace CasaDoCodigo.RelatorioWebAPI.Controllers
     [ApiController]
     public class RelatorioController : ControllerBase
     {
+        private readonly List<string> Relatorio = new List<string>() 
+        {
+            "Primeiro Pedido",
+            "Segundo Pedido"
+        };
+
         // GET: api/<RelatorioController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            StringBuilder stringBuilder = new StringBuilder();
 
-        // GET api/<RelatorioController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
+            foreach (var item in Relatorio)
+            {
+                stringBuilder.AppendLine(item);
+            }
+
+            return stringBuilder.ToString();
         }
 
         // POST api/<RelatorioController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
-        }
-
-        // PUT api/<RelatorioController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RelatorioController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            Relatorio.Add(value);
         }
     }
 }
