af95e6d786624c665603c6988792444b374b8ebb
Marcelo de Miranda Junior <marcelomijunior@outlook.com>
2020-07-21 15:24:57 -0300

    Criado controller de conta de usuário.

---
+++ CasaDoCodigo.MVC/Controllers/ContaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.MVC.Controllers
+{
+    public class ContaController : Controller
+    {
+        [Authorize]
+        public async Task<ActionResult> Login()
+        {
+            return Redirect(Url.Content("~/"));
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            // atualizar cookies (autenticacao local)
+            await HttpContext.SignOutAsync("Cookies");
+            // desconexão identity server
+            await HttpContext.SignOutAsync("OpenIdConnect");
+        }
+    }
+}
